77b1279451c21906be897c97b967cd3569cf69dc
abayomiodunayo <abayomiodunayo0@gmail.com>
2026-06-30 23:29:04 -1200

    Import sqlite3 in app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
+import sqlite3
 from werkzeug.security import generate_password_hash, check_password_hash
 from database import get_db_connection
 app = Flask(__name__)
